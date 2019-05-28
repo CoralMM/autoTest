813df61d12e03dbb5c34a6049d647bd093998a2b
CoralMM <1965154169@qq.com>
2019-05-28 16:55:51 +0800

    1

--- Ctest/src/main/java/com/t/c/Basic.java
+++ Ctest/src/main/java/com/t/c/Basic.java
@@ -1,4 +1,32 @@
 package com.t.c;
 
+
+import org.testng.annotations.*;
+
 public class Basic {
+    @BeforeSuite
+    public void beforSuite(){
+        System.out.println("BeforeSuite");
+    }
+    @Test
+    public void testCase1(){
+        System.out.println("测试之中");
+    }
+
+    @Test
+    public void testCase2(){
+        System.out.println("测试之中2");
+    }
+    @BeforeMethod
+    public void beforMethod(){
+        System.out.println("测试之前");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("测试之后");
+    }
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("AfterSuite");
+    }
 }
