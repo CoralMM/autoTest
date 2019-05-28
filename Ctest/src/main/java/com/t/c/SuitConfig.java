4cb50dc10d470002193555f89075b5015beb7c17
CoralMM <1965154169@qq.com>
2019-05-28 16:56:36 +0800

    2

--- Ctest/src/main/java/com/t/c/SuitConfig.java
+++ Ctest/src/main/java/com/t/c/SuitConfig.java
@@ -1,4 +1,25 @@
 package com.t.c;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
 public class SuitConfig {
+    @BeforeSuite
+    public void beforSuite(){
+        System.out.println("befor suite 运行啦");
+    }
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("after suite 运行啦");
+    }
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println("beforeTest");
+    }
+    @AfterTest
+    public void afterTest(){
+        System.out.println("afterTest");
+    }
 }
